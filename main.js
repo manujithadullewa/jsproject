5045915b930b9e23baed299d382f406376246e72
manujithadullewa <94696464+manujithadullewa@users.noreply.github.com>
2022-01-21 08:37:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// const isPalindrome = (x) => {
+//     let x_as_string = x.toString();
+//     let letters = [];
+//     let rnum = "";
+
+//     for(let i = 0; i < x_as_string.length; i++) {
+//         letters.push(x_as_string[i]);
+//     }
+
+//     for(let i = 0; i < x_as_string.length; i++) {
+//         rnum += letters.pop();
+//     }
+
+//     return parseInt(x_as_string) === parseInt(rnum)
+// }
+
+
+// console.log(isPalindrome(112345543211));
